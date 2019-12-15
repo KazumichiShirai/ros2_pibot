1780d84edd4cce65e36e5bc4fe84e20147b5a61f
KS <shirai@whill.jp>
2019-12-15 18:43:00 +0900

    Add TB6612Driver and modify Cmake for pigpio

---
+++ src/tb6612_driver.cpp
@@ -0,0 +1,58 @@
+#include <math.h>
+#include <tb6612_driver.hpp>
+
+unsigned int in1;
+unsigned int in2;
+unsigned int pwm;
+unsigned int stby;
+
+TB6612Driver::TB6612Driver(MotorPosition motor_position)
+{
+    if(motor_position == RIGHT_MOTOR){
+        in1 = 2;
+        in2 = 3;
+        pwm = 4;
+        stby = 17;
+    }else{
+        in1 = 14;
+        in2 = 15;
+        pwm = 18;
+        stby = 23;
+    }
+
+}
+
+void TB6612Driver::rotate(float v){
+    if(v > 0){ //CW
+        gpioWrite(in1, 1);
+        gpioWrite(in2, 0);
+        gpioWrite(stby, 1);
+    }else if(v < 0){//CCW
+        gpioWrite(in1, 0);
+        gpioWrite(in2, 1);
+        gpioWrite(stby, 1);
+    }else if(v == 0){
+        gpioWrite(in1, 0);
+        gpioWrite(in2, 0);
+        gpioWrite(stby, 1);
+    }else{
+        gpioWrite(stby, 0);
+    }
+
+    if(fabs(v) > 1.0){
+        //debug print
+        return;
+    }
+
+    unsigned dutycycle; //range:[0:255]
+    dutycycle = (unsigned)(v * 255.0);
+    gpioPWM(pwm, dutycycle);
+}
+
+TB6612Driver::~TB6612Driver(void)
+{
+    gpio(stby, 0);
+    gpio(in1, 0);
+    gpio(in2, 0);
+    gpio(in3, 0);
+}
---
+++ src/tb6612_driver.hpp
@@ -0,0 +1,13 @@
+#include <pigpio.h>
+
+enum class MotorPosition {RIGHT_MOTOR, LEFT_MOTOR};
+
+class TB6612Driver
+{
+private:
+
+public:
+    TB6612Driver(MotorPosition motor_position);
+    void rotate(float v);
+    ~TB6612Driver();
+}
