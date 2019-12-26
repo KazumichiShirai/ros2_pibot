89e38a0ebf25a5e8fe6fcf54b1ca7f81b39b4105
KS <shirai@whill.jp>
2019-12-26 21:27:41 +0900

    Swap left and right motor setting

--- src/tb6612_driver.cpp
+++ src/tb6612_driver.cpp
@@ -10,15 +10,15 @@ TB6612Driver::TB6612Driver(MotorPosition motor_position)
     static std::once_flag flag;
     std::call_once(flag, gpioInitialise);
     if(motor_position == MotorPosition::RIGHT_MOTOR){
-        in1 = 2;
-        in2 = 3;
-        pwm = 4;
-        stby = 17;
-    }else{
         in1 = 14;
         in2 = 15;
         pwm = 18;
         stby = 23;
+    }else{
+        in1 = 2;
+        in2 = 3;
+        pwm = 4;
+        stby = 17;
     }
     gpioSetMode(in1, PI_OUTPUT);
     gpioSetMode(in2, PI_OUTPUT);
